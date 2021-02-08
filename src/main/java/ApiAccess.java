cf6d021476920b548069fe7f1fbf76a1493bad55
Jana Kaszyda <jana.kaszyda@s2012.tu-chemnitz.de>
2021-02-08 11:37:41 +0100

    Initial commit

---
+++ src/main/java/ApiAccess.java
@@ -0,0 +1,11 @@
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+public class ApiAccess {
+
+    public static void main(String[] args) {
+
+     
+    }
+}
